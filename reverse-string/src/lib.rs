d2c07a87e6234e6a93870d2a5dd9f81f55842529
Cristiano <cristianocavo@me.com>
2019-09-05 16:52:59 +0200

    Update lib.rs

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,6 @@
+
+
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+    let drow: String = input.chars().rev().collect::<String>();
+    return drow;
 }
